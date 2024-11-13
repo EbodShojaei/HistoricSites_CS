23d0f854c1666fbb21a675dca60e0c1974aaf4c1
Ebod Shojaei <ebod.shojaei@gmail.com>
2024-11-13 02:13:33 -0800

    (update) sites map and cards render

--- ExoticHistoricSites.Client/Program.cs
+++ ExoticHistoricSites.Client/Program.cs
@@ -1,4 +1,5 @@
 using ExoticHistoricSites.Client;
+using ExoticHistoricSites.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,9 +7,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Configure HttpClient with base address
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
+    BaseAddress = new Uri("http://localhost:5054/"),
 });
+builder.Services.AddScoped<HistoricSiteService>();
 
 await builder.Build().RunAsync();
